6da28688bd1c14fc73f2f3689e8f4b564391fb23
Waddle986 <samonteleo4@gmail.com>
2025-09-05 09:03:35 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Leo";
+    cout << "your name is " << name << endl;
+
+    int num = 67;
+    cout << "your number is " << num << endl;
+
+    float numtwo = 28.3;
+    cout << "Your float is " << numtwo << endl;
+
+    double numthree = 34.647;
+    cout << "Your double is " << numthree << endl;
+
+    char mmm = 'A';
+    cout <<"your letter is " << mmm << endl;
+
+    bool go = "true";
+    cout << "You bool is equal to " << go << endl;
+}
